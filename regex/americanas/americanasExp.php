e5a79a0ca5fb1adae65f95e7ddc71080131df2c7
Alan Sparremberger <alan.rs@outlook.com>
2021-12-31 13:28:33 -0300

    inciando o scraping para o site da Americanas

---
+++ regex/americanas/americanasExp.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once './regex/Exp.php';
+
+class AmericanasExp extends Expression
+{
+    private $exp = array();
+
+    function __construct()
+    {
+    }
+
+    public function run($rawData)
+    {
+    }
+
+    protected function output($output)
+    {
+    }
+}
